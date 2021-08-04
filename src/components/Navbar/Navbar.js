e07593ad97132b21ea33fc9026b3aab980d60d7a
Mahier Sydow <mahier.sydow8@gmail.com>
2021-08-04 16:07:34 +0900

    navbar links/text-decoration

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,28 @@
 import React from "react";
 import "./navbar.css";
+import { Link } from "react-router-dom";
+
 
 function Navbar() {
   return (
     <div className="navbar">
       <div className="nav-wrapper">
-        <div className="nav-logo">hello</div>
+        <div className="nav-logo">
+          <Link to="/" className="link">logo</Link>
+        </div>
         <div className="nav-links-section">
-          <div className="nav-links">hello</div>
-          <div className="nav-links">hello</div>
-          <div className="nav-links">hello</div>
-          <div className="nav-links">hello</div>
+          <div className="nav-links">
+            <Link to="/" className="link">link 1</Link>
+          </div>
+          <div className="nav-links">
+            <Link to="/" className="link">link 2</Link>
+          </div>
+          <div className="nav-links">
+            <Link to="/" className="link">link 3</Link>
+          </div>
+          <div className="nav-links">
+            <Link to="/" className="link">link 4</Link>
+          </div>
         </div>
         <div className="nav-menu">hello</div>
       </div>
